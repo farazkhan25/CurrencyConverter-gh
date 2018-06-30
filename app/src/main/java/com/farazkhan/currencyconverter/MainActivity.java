8ed4e3eaa853c1e510bb8e5543d2b76b89d8bf0f
farazkhan25 <40715687+farazkhan25@users.noreply.github.com>
2018-06-30 17:06:19 +0530

    Update MainActivity.java

--- app/src/main/java/com/farazkhan/currencyconverter/MainActivity.java
+++ app/src/main/java/com/farazkhan/currencyconverter/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     public void clickFunction(View view) {
         EditText input = findViewById(R.id.input);
         double inputdouble = Double.parseDouble(input.getText().toString());
-        double result = inputdouble * 67.46;
+        double result = inputdouble * 68.45; //As of June 2018
         DecimalFormat df = new DecimalFormat("#.##");
         Toast.makeText(MainActivity.this, "₹ " + df.format(result), Toast.LENGTH_LONG).show();
     }
